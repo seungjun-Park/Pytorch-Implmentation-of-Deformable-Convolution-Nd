263e70d22866894d9c8f3ddda6200669d947bf09
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2024-11-09 04:14:04 +0900

    add example.

---
+++ example.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+from deform_conv import DeformConv2d
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+dtype = torch.bfloat16
+
+inputs = torch.randn(2, 384, 64, 64).to(device)
+
+deform_conv_layer = DeformConv2d(
+    in_channels=384,
+    out_channels=128,
+    kernel_size=3,
+    stride=1,
+    padding=1,
+    dilation=1,
+    groups=8,
+    deformable_groups_per_groups=2,
+    bias=True
+).to(device)
+
+with torch.autocast(device_type=device, dtype=dtype):
+    output = deform_conv_layer(inputs)
+
+print(output)
+"""
+result is 
+ 1 2 3 4 ..... each values
+ ...    ....     ........
+ 
+ device='cuda: 0', dtype=torch.bfloat16,
+ grad_fn=<CppNode<class DeformConvNdFunction<2>>>)
+ DeformConvNdFunction located in src/deform_conv.cpp
+"""
