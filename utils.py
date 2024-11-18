edf72f14bfd2e857d3f7b863f58f27f94634fed9
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2024-11-18 18:49:05 +0900

    add group_norm and get_act function.

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import torch
+import torch.nn as nn
 from collections import abc
 
 from itertools import repeat
@@ -27,3 +29,43 @@ def multiply_integers(x) -> int:
 
     return int(mul)
 
+
+def group_norm(num_channels, num_groups=32, eps=1e-6, affine=True):
+    return nn.GroupNorm(num_groups=num_groups, num_channels=num_channels, eps=eps, affine=affine)
+
+
+def get_act(name='relu', *args, **kwargs):
+    name = name.lower()
+
+    if name == 'relu':
+        return nn.ReLU(*args, **kwargs)
+
+    elif name == 'softplus':
+        return nn.Softplus(*args, **kwargs)
+
+    elif name == 'silu':
+        return nn.SiLU(*args, **kwargs)
+
+    elif name == 'sigmoid':
+        return nn.Sigmoid(*args, **kwargs)
+
+    elif name == 'tanh':
+        return nn.Tanh(*args, **kwargs)
+
+    elif name == 'hard_tanh':
+        return nn.Hardtanh(*args, **kwargs)
+
+    elif name == 'leaky_relu':
+        return nn.LeakyReLU(*args, **kwargs)
+
+    elif name == 'elu':
+        return nn.ELU(*args, **kwargs)
+
+    elif name == 'gelu':
+        return nn.GELU(*args, **kwargs)
+
+    elif name == 'mish':
+        return nn.Mish(*args, **kwargs)
+
+    else:
+        NotImplementedError(f'Activation function "{name}" is not supported.')
